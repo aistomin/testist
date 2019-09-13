257ba1ad328e889dfd4d8939df04b4bfed3f49c4
aistomin <andrej.istomin.ikeen@gmail.com>
2019-09-13 12:19:46 +0200

    Issue #16. TODOs to fix Qulice errors were added.

--- src/main/java/com/github/aistomin/testist/simple/SimpleQuestion.java
+++ src/main/java/com/github/aistomin/testist/simple/SimpleQuestion.java
@@ -29,6 +29,11 @@ import org.json.simple.JSONObject;
  * @since 0.1
  * @todo: Let's fix  Issue #20 and remove checkstyle suppression.
  * @todo: Let's fix  Issue #21 and remove PMD suppression.
+ * @todo: Let's fix  Issue #22 and remove PMD suppression.
+ * @todo: Let's fix  Issue #23 and remove PMD suppression.
+ * @todo: Let's fix  Issue #24 and remove PMD suppression.
+ * @todo: Let's fix  Issue #25 and remove PMD suppression.
+ * @todo: Let's fix  Issue #26 and remove PMD suppression.
  * @checkstyle AnnotationUseStyleCheck (200 lines)
  */
 @SuppressWarnings(
