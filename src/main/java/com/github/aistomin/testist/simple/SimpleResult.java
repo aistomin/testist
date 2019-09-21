76ebaa7d3846635d580d64012a74a8a6e14081b7
aistomin <andrej.istomin.ikeen@gmail.com>
2019-09-21 07:30:52 +0200

    Issue #33. SimpleResult.java: AnnotationUseStyleCheck checkstyle warning is solved.

--- src/main/java/com/github/aistomin/testist/simple/SimpleResult.java
+++ src/main/java/com/github/aistomin/testist/simple/SimpleResult.java
@@ -37,8 +37,6 @@ import org.json.simple.JSONObject;
  * @checkstyle BooleanExpressionComplexityCheck (200 lines)
  * @todo: Let's fix  Issue #32 and remove checkstyle suppression.
  * @checkstyle ExecutableStatementCountCheck (200 lines)
- * @todo: Let's fix  Issue #33 and remove checkstyle suppression.
- * @checkstyle AnnotationUseStyleCheck (200 lines)
  * @todo: Let's fix  Issue #35 and remove PMD suppression.
  * @todo: Let's fix  Issue #36 and remove PMD suppression.
  * @todo: Let's fix  Issue #37 and remove PMD suppression.
