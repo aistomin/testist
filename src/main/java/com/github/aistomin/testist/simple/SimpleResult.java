468c31a2054462b718461a19b0c8377f3838b4c3
aistomin <andrej.istomin.ikeen@gmail.com>
2019-09-26 08:17:31 +0200

    Issue #30. SimpleResult.java: NPathComplexityCheck warning is solved.

--- src/main/java/com/github/aistomin/testist/simple/SimpleResult.java
+++ src/main/java/com/github/aistomin/testist/simple/SimpleResult.java
@@ -26,8 +26,6 @@ import org.json.simple.JSONObject;
  * is correct.
  *
  * @since 0.1
- * @todo: Let's fix  Issue #30 and remove checkstyle suppression.
- * @checkstyle NPathComplexityCheck (200 lines)
  * @todo: Let's fix  Issue #31 and remove checkstyle suppression.
  * @checkstyle BooleanExpressionComplexityCheck (200 lines)
  * @todo: Let's fix  Issue #32 and remove checkstyle suppression.
